9ea43ee6be2f8adb7104283a0e62f07cd70ec5dc
matan <matan@iammaran.com>
2024-09-05 20:09:26 +0300

    fixed ai

--- nodes/http_serving.py
+++ nodes/http_serving.py
@@ -4,6 +4,9 @@ from collections import deque
 import json
 import base64
 from io import BytesIO
+from .utils import tensorToImageConversion
+from PIL import Image
+
 
 class HTTPServing:
     def __init__(self):
@@ -12,6 +15,8 @@ class HTTPServing:
         self.http_running = False
         self.port = None
         self.server = None
+        self.output_ready = threading.Event()
+        self.output = None
 
     def http_handler(self):
         class RequestHandler(BaseHTTPRequestHandler):
@@ -22,17 +27,22 @@ class HTTPServing:
                 self.data.append(data)
                 self.data_ready.set()
 
+                self.output_ready.wait()
+                response = self.output
                 self2.send_response(200)
                 self2.send_header('Content-type', 'application/json')
                 self2.end_headers()
-                self2.wfile.write(json.dumps({"status": "received"}).encode('utf-8'))
+                self2.wfile.write(json.dumps(response).encode('utf-8'))
+
+
+
 
             def do_GET(self2):
                 if self2.path == '/':
                     self2.send_response(200)
                     self2.send_header('Content-type', 'text/html')
                     self2.end_headers()
-                    self2.wfile.write(b"HTTP Serving is running")
+                    self2.wfile.write(b"HTTP Serving is running, to send data make a POST request to this endpoint.")
 
         self.server = HTTPServer(('', self.port), RequestHandler)
         print(f"HTTP Server running on port {self.port}")
@@ -77,21 +87,29 @@ class HTTPServing:
                 base64_images.append(base64_img)
             response = {
                 "base64_images": base64_images,
-                "_requestId": data["_requestId"]
             }
-            return json.dumps(response)
+            self.output = response
+            self.output_ready.set()
+
 
         def serve_image_function(image, frame_duration):
             image_file = tensorToImageConversion(image, frame_duration)
             base64_img = base64.b64encode(image_file.read()).decode('utf-8')
             response = {
                 "base64_img": base64_img,
-                "_requestId": data["_requestId"]
             }
-            return json.dumps(response)
+            self.output = response
+            self.output_ready.set()
+
+        def serve_text_function(text):
+            response = {
+                "text": text,
+            }
+            self.output = response
+            self.output_ready.set()
 
         data["serve_image_function"] = serve_image_function
         data["serve_multi_image_function"] = serve_multi_image_function
-        data["serve_text_function"] = lambda text: json.dumps({"text": text, "_requestId": data["_requestId"]})
+        data["serve_text_function"] = serve_text_function
 
         return (data,)
